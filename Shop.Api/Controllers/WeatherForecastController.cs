e0213da954e64981b2fb37547fde92d806ccc2e5
Francis Ricardo Ramirez <francisricardor@gmail.com>
2020-11-11 21:19:57 -0400

    Agregando API de los servicios.

---
+++ Shop.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Shop.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var lst = new List<WeatherForecast>() 
+            {
+                new WeatherForecast(){  Id=1, Nombre="Francis Ramirez"},
+                new WeatherForecast(){  Id=2, Nombre="Jose Perez"},
+                new WeatherForecast(){  Id=3, Nombre="Juan Garcia"},
+            };
+
+            return lst;
+             
+        }
+    }
+}
